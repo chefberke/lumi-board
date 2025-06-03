c9af328627d735f4caf3b4ae1197ca4d5303d830
Berke Kanber <berkekanber@gmail.com>
2025-06-03 21:38:24 +0300

    refactor(dashboard): remove ThemeToggle component and replace with placeholder text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import ThemeToggle from "@/components/shared/Theme";
 import React from "react";
 
 function Dashboard() {
@@ -6,9 +5,7 @@ function Dashboard() {
     <div>
       <h1 className="text-2xl font-bold mb-4">Home</h1>
 
-      <div className="w-full relative pb-8">
-        <ThemeToggle />
-      </div>
+      <div className="w-full relative pb-8">deneme</div>
     </div>
   );
 }
