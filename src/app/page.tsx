77196690784adaa49d7b09c704561ef7a4dbe2a2
Berke Kanber <berkekanber@gmail.com>
2025-06-26 01:02:09 +0300

    refactor: simplify landing page by removing unused components and integrating Hero from the new UI library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Hero from "@/components/landing/Hero";
-import Nav from "@/components/landing/Nav";
-import Footer from "@/components/landing/Footer";
+import { Hero } from "@/components/ui/hero-main";
 
 export default function Home() {
   return (
-    <div className="flex ites-center justify-center w-full  h-screen">
-      <div className="flex flex-col h-full w-full max-w-[1100px]">
-        <Nav />
-        <Hero />
-        <Footer />
-      </div>
+    <div>
+      <Hero />
     </div>
   );
 }
